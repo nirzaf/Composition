5e05830ac0f57357ab6d14fc6a5daf5d25442f0a
nirzaf <mfmfazrin1986@gmail.com>
2022-01-04 17:55:54 +0530

    Channel channel = new Channel("localhost", 50051, ChannelCredentials.Insecure);// channelCredentials);

--- Program.cs
+++ Program.cs
@@ -12,46 +12,6 @@ class program
         
         Console.ReadKey(true);
     }
+
 }
 
-
-public class Vehicle
-{
-    private long Id { get; set; }
-    private string Name { get; set; }
-    private string Type { get; set; }
-    private string Color { get; set; }
-    private string Make { get; set; }
-    private string Model { get; set; }
-    private string Year { get; set; }
-    private string Plate { get; set; }
-    
-    public Vehicle()
-    {
-    }
-
-    public Vehicle(long id, string name, string type, string color, string make, string model, string year, string plate)
-    {
-        Id = id;
-        Name = name;
-        Type = type;
-        Color = color;
-        Make = make;
-        Model = model;
-        Year = year;
-        Plate = plate;
-    }
-
-    public void Drive()
-    {
-        Console.WriteLine("The vehicle is driving");
-    }
-}
-
-public class Car : Vehicle
-{
-    public void NewCar()
-    {
-        Console.WriteLine("The car is new");
-    }
-}
