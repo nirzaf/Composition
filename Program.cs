15a532ca53649a1377cb52545f7cfe230dfacfd6
nirzaf <mfmfazrin1986@gmail.com>
2022-02-26 18:36:45 +0530

    WriteLine($"Building engine with type: {_engineType}, & size: {_engineSize}");

--- Program.cs
+++ Program.cs
@@ -1,24 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+global using static System.Console;
 class program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        Console.WriteLine("Press any key to continue...");
+        WriteLine("Hello World!");
+        WriteLine("Press any key to continue...");
         
         var c = new Car();
         c.NewCar();
-        
-        Console.ReadKey(true);
+        ReadKey(true);
     }
-
 }
 
 public class Car
 {
+    private Engine eng = new Engine("QWER","Auto");
     public void NewCar()
     {
-        Console.WriteLine("This is a new car.........!");
+        WriteLine("This is a new car.........!");
+        eng.BuildEngine();
+    }
+}
+
+public class Engine
+{
+    private readonly string _engineType;
+    private readonly string _engineSize;
+    public Engine(string engineSize, string engineType)
+    {
+        _engineSize = engineSize;
+        _engineType = engineType;
+    }
+    
+    public void BuildEngine()
+    {
+        WriteLine($"Building engine with type: {_engineType}, & size: {_engineSize}");
     }
 }
