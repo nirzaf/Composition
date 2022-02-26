d8a87899d619a4d690c273c21e6d444653f20044
nirzaf <mfmfazrin1986@gmail.com>
2022-02-26 18:37:34 +0530

    WriteLine($"Building engine with type: {_engineType}, & size: {_engineSize}");

---
+++ Engine.cs
@@ -0,0 +1,15 @@
+﻿public class Engine
+{
+    private readonly string _engineType;
+    private readonly string _engineSize;
+    public Engine(string engineSize, string engineType)
+    {
+        _engineSize = engineSize;
+        _engineType = engineType;
+    }
+    
+    public void BuildEngine()
+    {
+        WriteLine($"Building engine with type: {_engineType}, & size: {_engineSize}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -22,19 +22,3 @@ public class Car
         eng.BuildEngine();
     }
 }
-
-public class Engine
-{
-    private readonly string _engineType;
-    private readonly string _engineSize;
-    public Engine(string engineSize, string engineType)
-    {
-        _engineSize = engineSize;
-        _engineType = engineType;
-    }
-    
-    public void BuildEngine()
-    {
-        WriteLine($"Building engine with type: {_engineType}, & size: {_engineSize}");
-    }
-}
